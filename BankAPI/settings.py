5d717c55723d0c80fe0556f3616c78c521e1b488
Udokaamos <udokaamos@gmail.com>
2022-07-23 21:58:28 +0100

    requirement

--- BankAPI/settings.py
+++ BankAPI/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =  os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['gloria-bank-app2.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
