1001d01695e9be3c43a215d7ce456e7afb948933
Udokaamos <udokaamos@gmail.com>
2022-07-25 16:10:24 +0100

    settings

--- BankAPI/settings.py
+++ BankAPI/settings.py
@@ -15,7 +15,8 @@ from dotenv import load_dotenv, find_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv(find_dotenv())
 
@@ -23,7 +24,7 @@ load_dotenv(find_dotenv())
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv('SECRET_KEY')
+SECRET_KEY =  os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
