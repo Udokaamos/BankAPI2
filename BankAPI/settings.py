067c6ac0455b8ec3b634375d18875d3c70035ea1
Udokaamos <udokaamos@gmail.com>
2022-07-25 23:25:21 +0100

    allowed host

--- BankAPI/settings.py
+++ BankAPI/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =  os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['glow-bank-api.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 REST_FRAMEWORK = {
     
