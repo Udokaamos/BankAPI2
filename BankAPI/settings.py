1b38b2af5d00c6ada43951ad629771f422338025
Udokaamos <udokaamos@gmail.com>
2022-07-27 21:47:03 +0100

    changes

--- BankAPI/settings.py
+++ BankAPI/settings.py
@@ -17,8 +17,8 @@ from dotenv import load_dotenv, find_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv(find_dotenv())
 
