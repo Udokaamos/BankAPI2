52a2c49aa392718445ea2c6a6382f1755d478dfa
Udokaamos <udokaamos@gmail.com>
2022-07-28 19:50:00 +0100

    settings

--- BankAPI/settings.py
+++ BankAPI/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from pathlib import Path
+# from pathlib import Path
 import django_heroku
-import dj_database_url
+# import dj_database_url
 from dotenv import load_dotenv, find_dotenv
 
 
